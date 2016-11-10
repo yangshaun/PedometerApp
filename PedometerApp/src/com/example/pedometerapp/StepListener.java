ab1eb05a6427361013d62865fe3464ba7adb8a27
yangshaun <aberfoule@yahoo.com.tw>
2016-11-10 13:09:43 +0800

    PedometerAPP

--- PedometerApp/src/com/example/pedometerapp/StepListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.pedometerapp;
-
-public interface StepListener {
-	 /**
-	   * Called when a step has been detected.  Given the time in nanoseconds at
-	   * which the step was detected.
-	   */
-	  public void step(long timeNs);
-}
